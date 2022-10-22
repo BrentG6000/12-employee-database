d1af37d55bb6b4e88f3470519e06ccad816a368c
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-10-21 23:10:23 -0500

    Added code for schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,14 +4,15 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT AUTO_INCREMENT PRIMARY KEY,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
+  department_id INT,
   FOREIGN KEY (department_id)
   REFERENCES department(id)
   ON DELETE SET NULL
@@ -20,11 +21,13 @@ CREATE TABLE role (
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT,
   FOREIGN KEY (role_id)
   REFERENCES role(id)
-  ON DELETE SET NULL
+  ON DELETE SET NULL,
   FOREIGN KEY (manager_id)
-  REFERENCES manager(id)
+  REFERENCES employee(id)
   ON DELETE SET NULL
 );

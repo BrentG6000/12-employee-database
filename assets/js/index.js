fec23c04abbca61357165374a0e11315da213a7d
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-10-23 03:29:01 -0500

    Added db class file

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,178 @@
 const mysql = require('mysql2');
 const cTable = require('console.table');
-const inquirer = require('inquirer');
-const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'newuser',
-        password: '12345!',
-        database: 'employee_db'
-    },
-    console.log(`Connected to the employee_db database.`)
-);
+const db = require("./db");
+const { prompt } = require("inquirer");
+const logo = require("asciiart-logo");
 
-const questions = [
-    {
-        type: 'list',
-        message: 'Choose an option from the list:',
-        name: 'option',
-        choices: [
-            'View all departments',
-            'View all roles',
-            'View all employees',
-            'Add a department',
-            'Add a role',
-            'Add an employee',
-            'Update an employee role',
-        ],
+// Here is a function which handles the first prompt option:  View all employees
+function viewEmployees() {
+
+    // Here we call the method in the db file for finding all employees.
+    // we get the result back, and then display the result 
+    db.findAllEmployees()
+        .then(([rows]) => {
+            let employees = rows;
+            console.log("\n");
+            console.table(employees);
+        })
+        .then(() => loadMainPrompts());
+}
+
+function viewRoles() {
+
+    // Here we call the method in the db file for finding all employees.
+    // we get the result back, and then display the result 
+    db.findAllEmployees()
+        .then(([rows]) => {
+            let employees = rows;
+            console.log("\n");
+            console.table(employees);
+        })
+        .then(() => loadMainPrompts());
+    
+    db.query(
+        'SELECT * FROM `role`',
+        function (err, results) {
+            if (err) {
+                console.log("Error retrieving employee role table.");
+            }
+            else {
+                const table = cTable.getTable('Roles', results);
+                console.log(table);
+            }
+        }
+    );
+}
+
+function viewEmployees() {
+
+    // Here we call the method in the db file for finding all employees.
+    // we get the result back, and then display the result 
+    db.findAllEmployees()
+        .then(([rows]) => {
+            let employees = rows;
+            console.log("\n");
+            console.table(employees);
+        })
+        .then(() => loadMainPrompts());
+}
+
+// Here we load the initial prompts with a series of options. The first option is provided for you.
+function loadMainPrompts() {
+    prompt([
+        {
+            type: "list",
+            name: "choice",
+            message: "What would you like to do?",
+            choices: [
+                {
+                    name: "View All Employees",
+                    value: "VIEW_EMPLOYEES"
+                },
+                {
+                    name: "View All Roles",
+                    value: "VIEW_ROLES"
+                },
+                {
+                    name: "View All Departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
+                {
+                    name: "Add A Department",
+                    value: "ADD_DEPARTMENT"
+                },
+                {
+                    name: "Add A Role",
+                    value: "ADD_ROLE"
+                },
+                {
+                    name: "Add An Employee",
+                    value: "ADD_EMPLOYEE"
+                },
+                {
+                    name: "Update An Employee Role",
+                    value: "UPDATE_ROLE"
+                },
+            ]
+        }
+    ]).then(res => {
+        let choice = res.choice;
+        // Call the appropriate function depending on what the user chose
+
+        switch (choice) {
+            case "VIEW_EMPLOYEES":
+                viewEmployees();
+                break;
+            case 'VIEW_ROLES':
+                
+                break;
+            case 'View all employees':
+                let roleResults =
+                    db.query(
+                        'SELECT * FROM `role`',
+                        function (err, results) {
+                            if (err) {
+                                console.log("Error retrieving employee role table.");
+                            }
+                            else {
+                                return results;
+                            }
+                        }
+                    );
+
+                db.query(
+                    'SELECT * FROM `employee`',
+                    function (err, results) {
+                        if (err) {
+                            console.log("Error retrieving employee table.");
+                        }
+                        else {
+                            let newResults = [];
+                            let tempEmployee = {
+                                ID: 0,
+                                First_Name: '',
+                                Last_Name: '',
+                                Role: '',
+                                Manager: ''
+                            };
+                            let role;
+                            let manager;
+                            for (let i = 0; i < results.length; i++) {
+                                tempEmployee.ID = results[i].id;
+                                tempEmployee.First_Name = results[i].first_name;
+                                tempEmployee.Last_Name = results[i].last_name;
+                                tempEmployee.Role = roleResults[results[i].role_id]
+                                if (results[i].manager_id) {
+                                    tempEmployee. = results[results[i].manager_id].fist_name + 
+                                    }
+                            }
+                            const table = cTable.getTable('Employees', results);
+                            console.log(table);
+                        }
+                    }
+                );
+                break;
+            case 'Add a department':
+                console.log('Add dept');
+                break;
+            case 'Add a role':
+                console.log('Add role');
+                break;
+            case 'Add an employee':
+                console.log('Add employee');
+                break;
+            case 'Update an employee role':
+                console.log('Update role');
+                break;
+            default:
+                console.log('default');
+        };
+
+            // add the other case statements here
+        }
     }
-];
+    )
+}
 
 function init() {
     inquirer
@@ -47,51 +193,15 @@ function init() {
                         }
                     );
                     break;
-                case 'View all roles':
-                    db.query(
-                        'SELECT * FROM `role`',
-                        function (err, results) {
-                            if (err) {
-                                console.log("Error retrieving employee role table.");
-                            }
-                            else {
-                                const table = cTable.getTable('Roles', results);
-                                console.log(table);
-                            }
-                        }
-                    );
-                    break;
-                case 'View all employees':
-                    db.query(
-                        'SELECT * FROM `employee`',
-                        function (err, results) {
-                            if (err) {
-                                console.log("Error retrieving employee table.");
-                            }
-                            else {
-                                const table = cTable.getTable('Employees', results);
-                                console.log(table);
-                            }
-                        }
-                    );
-                    break;
-                case 'Add a department':
-                    console.log('Add dept');
-                    break;
-                case 'Add a role':
-                    console.log('Add role');
-                    break;
-                case 'Add an employee':
-                    console.log('Add employee');
-                    break;
-                case 'Update an employee role':
-                    console.log('Update role');
-                    break;
-                default:
-                    console.log('default');
-            };
+                
         })
 }
+
+function init() {
+
+    loadMainPrompts()
+}
+
 
 init();
 // const questions = [{
@@ -99,57 +209,13 @@ init();
 //     name: 'title',
 //     message: 'What is the title of your project?',
 // },
-// {
-//     type: 'input',
-//     message: 'Enter a description of the project:',
-//     name: 'description',
-// },
-// {
-//     type: 'input',
-//     message: 'Enter installation instructions:',
-//     name: 'installation',
-// },
-// {
-//     type: 'input',
-//     message: 'Enter usage information:',
-//     name: 'usage',
-// },
-// {
-//     type: 'input',
-//     message: 'Enter contribution guidelines:',
-//     name: 'contribution',
-// },
-// {
-//     type: 'input',
-//     message: 'Enter test instructions:',
-//     name: 'test',
-// },
-// {
-//     type: 'input',
-//     message: 'Enter your GitHub username:',
-//     name: 'username',
-// },
-// {
-//     type: 'input',
-//     message: 'Enter your email address:',
-//     name: 'email',
-// },
-// ];
 
-// // Query database using COUNT() and GROUP BY
-// db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
-//     console.log(results);
-// });
+// /* ======= Controllers ============================================================ */
 
-// // Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
-// db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
-//     console.log(results);
-// });
+// /* ======= END Controllers ============================================================ */
 
-// app.use((req, res) => {
-//     res.status(404).end();
-// });
-
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
+/* 
+You will write lots of other functions here for the other prompt options.
+Note that some prompts will require you to provide more prompts, and these 
+may need functions of their own.
+ */

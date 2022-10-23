6047020f5df941a19099b2d1e4f50cc94dc82cb1
Brent Gustafson <50030696+BrentG6000@users.noreply.github.com>
2022-10-22 20:47:21 -0500

    Added code to index.js to print out tables

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,15 @@
+const mysql = require('mysql2');
+const cTable = require('console.table');
 const inquirer = require('inquirer');
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'newuser',
+        password: '12345!',
+        database: 'employee_db'
+    },
+    console.log(`Connected to the employee_db database.`)
+);
 
 const questions = [
     {
@@ -17,6 +28,72 @@ const questions = [
     }
 ];
 
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) => {
+            switch (data.option) {
+                case 'View all departments':
+                    db.query(
+                        'SELECT * FROM `department`',
+                        function (err, results) {
+                            if (err) {
+                                console.log("Error retrieving department table.");
+                            }
+                            else {
+                                const table = cTable.getTable('Departments', results);
+                                console.log(table);
+                            }    
+                        }
+                    );
+                    break;
+                case 'View all roles':
+                    db.query(
+                        'SELECT * FROM `role`',
+                        function (err, results) {
+                            if (err) {
+                                console.log("Error retrieving employee role table.");
+                            }
+                            else {
+                                const table = cTable.getTable('Roles', results);
+                                console.log(table);
+                            }
+                        }
+                    );
+                    break;
+                case 'View all employees':
+                    db.query(
+                        'SELECT * FROM `employee`',
+                        function (err, results) {
+                            if (err) {
+                                console.log("Error retrieving employee table.");
+                            }
+                            else {
+                                const table = cTable.getTable('Employees', results);
+                                console.log(table);
+                            }
+                        }
+                    );
+                    break;
+                case 'Add a department':
+                    console.log('Add dept');
+                    break;
+                case 'Add a role':
+                    console.log('Add role');
+                    break;
+                case 'Add an employee':
+                    console.log('Add employee');
+                    break;
+                case 'Update an employee role':
+                    console.log('Update role');
+                    break;
+                default:
+                    console.log('default');
+            };
+        })
+}
+
+init();
 // const questions = [{
 //     type: 'input',
 //     name: 'title',
@@ -36,20 +113,6 @@ const questions = [
 //     type: 'input',
 //     message: 'Enter usage information:',
 //     name: 'usage',
-// },
-// {
-//     type: 'list',
-//     message: 'Choose an option from the list:',
-//     name: 'option',
-//     choices: [
-//         'View all departments',
-//         'View all roles',
-//         'View all employees',
-//         'Add a department',
-//         'Add a role',
-//         'Add an employee',
-//         'Update an employee role',
-//     ],
 // },
 // {
 //     type: 'input',
@@ -72,3 +135,21 @@ const questions = [
 //     name: 'email',
 // },
 // ];
+
+// // Query database using COUNT() and GROUP BY
+// db.query('SELECT COUNT(id) AS total_count FROM favorite_books GROUP BY in_stock', function (err, results) {
+//     console.log(results);
+// });
+
+// // Query database using SUM(), MAX(), MIN() AVG() and GROUP BY
+// db.query('SELECT SUM(quantity) AS total_in_section, MAX(quantity) AS max_quantity, MIN(quantity) AS min_quantity, AVG(quantity) AS avg_quantity FROM favorite_books GROUP BY section', function (err, results) {
+//     console.log(results);
+// });
+
+// app.use((req, res) => {
+//     res.status(404).end();
+// });
+
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
--- db/seeds.sql
+++ db/seeds.sql
@@ -15,12 +15,12 @@ VALUES ("Accountant", 6000.00, 1),
        ("Chief Operations Officer", 10000.00, 3);
        
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Brent", "Johnson", 3, 7),
-       ("John", "Doe", 1, 6),
-       ("Jim", "Thorne", 4, 7),
-       ("Sarah", "Lee", 4, 7),
-       ("Jane", "Doe", 5, 8),
-       ("Peter", "Zimmer", 7, null),
+VALUES ("Peter", "Zimmer", 7, null),
        ("Jessica", "Atell", 6, null),
        ("Jesse", "McDonald", 8, null),
-       ("Zach", "Black", 2, 4);
+       ("John", "Doe", 1, 2),
+       ("Jim", "Thorne", 4, 1),
+       ("Sarah", "Lee", 4, 1),
+       ("Jane", "Doe", 5, 3),
+       ("Brent", "Johnson", 3, 2),
+       ("Zach", "Black", 2, 2);
